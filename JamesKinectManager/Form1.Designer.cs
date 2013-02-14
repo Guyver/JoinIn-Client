d0396abbaa7b9315e79972e7ae5f480c844645ab
James Browne <James_Browne@live.ie>
2013-02-14 05:57:56 +0000

    New Version

---
+++ JamesKinectManager/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace JamesKinectManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_Run = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tb_Ip = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_Port = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_log = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Run
+            // 
+            this.btn_Run.Location = new System.Drawing.Point(334, 40);
+            this.btn_Run.Name = "btn_Run";
+            this.btn_Run.Size = new System.Drawing.Size(75, 23);
+            this.btn_Run.TabIndex = 0;
+            this.btn_Run.Text = "Run";
+            this.btn_Run.UseVisualStyleBackColor = true;
+            this.btn_Run.Click += new System.EventHandler(this.btn_Run_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP to listen to: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 274);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(254, 132);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tb_Ip
+            // 
+            this.tb_Ip.Location = new System.Drawing.Point(150, 40);
+            this.tb_Ip.Name = "tb_Ip";
+            this.tb_Ip.Size = new System.Drawing.Size(136, 20);
+            this.tb_Ip.TabIndex = 3;
+            this.tb_Ip.Text = "127.0.0.1";
+            this.tb_Ip.TextChanged += new System.EventHandler(this.tb_Ip_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter ip to listen to...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Enter port to listen on...";
+            // 
+            // tb_Port
+            // 
+            this.tb_Port.Location = new System.Drawing.Point(150, 149);
+            this.tb_Port.Name = "tb_Port";
+            this.tb_Port.Size = new System.Drawing.Size(136, 20);
+            this.tb_Port.TabIndex = 7;
+            this.tb_Port.Text = "8000";
+            this.tb_Port.TextChanged += new System.EventHandler(this.tb_Port_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Port to listen on: ";
+            // 
+            // lb_log
+            // 
+            this.lb_log.FormattingEnabled = true;
+            this.lb_log.Items.AddRange(new object[] {
+            "Socket events and log reporter..."});
+            this.lb_log.Location = new System.Drawing.Point(450, 40);
+            this.lb_log.Name = "lb_log";
+            this.lb_log.Size = new System.Drawing.Size(381, 251);
+            this.lb_log.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(899, 418);
+            this.Controls.Add(this.lb_log);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_Port);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_Ip);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Run);
+            this.Name = "Form1";
+            this.Text = "James\'s Kinect Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Run;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tb_Ip;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_Port;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lb_log;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ JamesKinectManager/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Fleck;
+using Microsoft.Research.Kinect.Nui;
+
+namespace JamesKinectManager
+{
+    public partial class Form1 : Form
+    {
+        // Are we listening at the minute?
+        private bool mListening = false;
+
+        // Port to listen to.
+        private string mPort = "8000";
+
+        // Ip address to listen to.
+        private string mIpaddress = "127.0.0.1";
+
+        // Microsofts jazz.
+        Runtime mNui = new Runtime();
+
+        Device mDevices = new Device();
+
+        // A list of sockets that are connected.
+        private List<IWebSocketConnection> mSockets;
+
+        // Has the device been initalised.
+        private bool mInitializedKinect = false;
+
+        // Has the device been initalised.
+        private bool mInitialisedSockets = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Timer set to 60fps.
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+
+        /// <summary>
+        /// Create the server from the ip and port entered by the user.
+        /// Defines the OnOpen, OnClose and OnMessage functions for fleck.
+        /// </summary>
+        private void InitializeSockets()
+        {
+            mSockets = new List<IWebSocketConnection>();
+
+            try
+            {
+                var server = new WebSocketServer("ws://" + mIpaddress + ":" + mPort);
+
+                server.Start(socket =>
+                {
+                    socket.OnOpen = () =>
+                    {
+                        lb_log.Items.Add("Connected to client : " + socket.ConnectionInfo.ClientIpAddress);
+                        mSockets.Add(socket);
+                    };
+
+
+                    socket.OnClose = () =>
+                    {
+                        lb_log.Items.Add("Disconnected from client : " + socket.ConnectionInfo.ClientIpAddress);
+                        mSockets.Remove(socket);
+                    };
+
+
+                    socket.OnMessage = message =>
+                    {
+                        lb_log.Items.Add("Message from client : " + message);
+                    };
+                });
+
+                lb_log.Items.Add("Server is initalised");
+                lb_log.Items.Add("Waiting to recieve data on : " + server.Location);
+
+                mInitialisedSockets = true;
+            }
+            catch(Exception e)
+            {
+                lb_log.Items.Add("Failed to initalise the server.");
+                lb_log.Items.Add("The error was : " + e.Message );
+            }
+        }
+
+
+        /// <summary>
+        /// Initalise the kinect and set it to track a skeleton.
+        /// Provide a callback function that will fire when our skeleton is ready each frame.
+        /// </summary>
+        private void InitilizeKinect()
+        {
+            if (mDevices.Count <= 0)
+            {
+                lb_log.Items.Add("There are no Kinects detected.");
+            }
+
+            try
+            {
+                // Initalise the kinect to track multiple skeletons.
+                mNui.Initialize( RuntimeOptions.UseDepthAndPlayerIndex | RuntimeOptions.UseSkeletalTracking );
+
+                // Add a new callback event to frame ready.
+                mNui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>( Nui_SkeletonFrameCallback );
+
+                // We're ready to go
+                mInitializedKinect = true;
+                lb_log.Items.Add("Kinect is initalised.");
+            }
+            catch(Exception e)
+            {
+                // Error reporting.
+                lb_log.Items.Add( "Error code : "+e.Message );
+                lb_log.Items.Add( "Try plugging in the Kinect and make sure the drivers/SDK is installed." );
+            }
+        }
+
+
+        /// <summary>
+        /// Triggered from inside the Kinect library each time a skeleton frame is ready. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Nui_SkeletonFrameCallback(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            if (!mInitializedKinect) return;
+
+            List<SkeletonData> users = new List<SkeletonData>();
+
+            foreach (var user in e.SkeletonFrame.Skeletons)
+            {
+                if ( user.TrackingState == SkeletonTrackingState.Tracked )
+                {
+                    users.Add( user );
+                }
+            }
+
+            if (users.Count > 0)
+            {
+                string json = users.ToString();
+
+                foreach (var socket in mSockets)
+                {
+                    socket.Send(json);
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tb_Ip_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tb_Port_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Run_Click(object sender, EventArgs e)
+        {
+            // Initalise the server.
+            if ( !mListening && !mInitialisedSockets )
+            {
+                // Store the IP address.
+                mIpaddress = tb_Ip.Text;
+                // Store the Port.
+                mPort = tb_Port.Text;
+
+                // Initalise the server using the data above.
+                InitializeSockets();                
+
+                // We're listening for data.
+                mListening = true;
+
+                // Change the button to stop.
+                btn_Run.Text = "Stop";
+            }
+            else
+            {
+                Application.Exit();
+            }
+
+            // Initalise the Kinect device.
+            if( !mInitializedKinect )
+            {
+                InitilizeKinect();
+            }
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (mDevices.Count <= 0)
+            {
+                lb_log.Items.Add("There are no Kinects detected. Did you unplug the kinect?");
+                mInitializedKinect = false;
+            }
+        }
+    }
+}
